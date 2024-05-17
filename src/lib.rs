dcdba268787f5a9512228318c51d483e0e950a38
starkbamse <139136798+starkbamse@users.noreply.github.com>
2024-05-17 21:16:18 +0200

    Export round via core

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@
 /// This library provides a simple interface to fetch price data from the Chainlink decentralized data feed.
 /// Core is the main module that contains the main struct `Rustlink` that you will need to interact with.
 pub mod core;
+mod interface;
 mod error;
 mod fetcher;
-mod interface;
 #[cfg(test)]
 mod tests {
 
